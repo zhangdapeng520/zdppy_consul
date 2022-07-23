8c61cf9863ec1e788b1ded5bef0803e4ccc951cb
zhangdapeng520 <pygosuperman@outlook.com>
2022-07-23 11:20:01 +0800

    新增：基本使用

---
+++ zdppy_consul/consul/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.1.0'
+
+from .std import Consul
+from .base import Check
+from .base import ConsulException
+from .base import ACLPermissionDenied
+from .base import ACLDisabled
+from .base import NotFound
+from .base import Timeout
